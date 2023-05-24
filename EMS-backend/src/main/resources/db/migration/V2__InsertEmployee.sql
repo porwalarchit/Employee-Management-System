b6c076fdc8e3b8ce3f00dbe8c6c58d0da04b9ad6
porwalarchit <architporwal@gmail.com>
2023-05-24 18:44:32 +0530

    Added flyway sql queries to create Tables and insert employee data.

---
+++ EMS-backend/src/main/resources/db/migration/V2__InsertEmployee.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.employee(
+    id, email, first_name, last_name, password, roles)
+    VALUES (1, 'aporwal@argusoft.com', 'Archit', 'Porwal', '$2a$10$0EcD8ylytscGoT5FgCvS/OYX7RtHDKtuiHY8T8gUnXu1khrV7mk.q', 'ROLE_SUPERADMIN'),
+     (2,'archit@gmail.com', 'Archit', 'Porwal', '$2a$10$WaTXosRc2gm4YB6.B5WuIOYAZ4WT8AA7K86.rfmWBL8QYN/DZOi7u', 'ROLE_ADMIN'),
+     (3,'architporwal@gmail.com', 'Archit', 'Porwal', '$2a$10$zCYVCWF8kCY8JZIPBoppreyAiju/vSqoLyVDgacnHiLHUUYvLX0G2', 'ROLE_EMPLOYEE');
