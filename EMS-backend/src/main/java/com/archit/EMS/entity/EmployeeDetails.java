cb883cbcbf41907aa7f5fcc905a077386f7ad041
porwalarchit <architporwal@gmail.com>
2023-05-24 18:42:51 +0530

    Employee Details entity class

---
+++ EMS-backend/src/main/java/com/archit/EMS/entity/EmployeeDetails.java
@@ -0,0 +1,43 @@
+package com.archit.EMS.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "employee_details")
+public class EmployeeDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_md_id")
+    private int empMdId;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "contact_details")
+    private String contactNumber;
+
+    @Column(name = "designation")
+    private String designation;
+
+    @Column(name = "joining_date")
+    private Date joiningDate;
+
+    @Column(name = "salary")
+    private int salary;
+
+//    @OneToOne(mappedBy = "employee_details")
+//    private Employee employee;
+}
