1d5c383ea35758d970391ca558dafd4d1d3282e1
porwalarchit <architporwal@gmail.com>
2023-05-29 18:37:03 +0530

    Employee Report controller having GET & POST request endpoints.

---
+++ EMS-backend/src/main/java/com/archit/EMS/controller/EmployeeReportController.java
@@ -0,0 +1,32 @@
+package com.archit.EMS.controller;
+
+import com.archit.EMS.entity.EmployeeReport;
+import com.archit.EMS.service.EmployeeReportService;
+import com.archit.EMS.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/report")
+public class EmployeeReportController {
+    @Autowired
+    private EmployeeReportService employeeReportService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @PostMapping("addreport")
+    public EmployeeReport addReport(@RequestBody EmployeeReport theEmployeeReport){
+        theEmployeeReport.setEmployee(employeeService.findEmployeeByEmail(theEmployeeReport.getEmployee().getEmail()).get());
+        return employeeReportService.saveReport(theEmployeeReport);
+    }
+
+    @GetMapping("{id}")
+    public Optional<EmployeeReport> getReport(@PathVariable int id){
+        return employeeReportService.getEmployeeReport(id);
+    }
+}
