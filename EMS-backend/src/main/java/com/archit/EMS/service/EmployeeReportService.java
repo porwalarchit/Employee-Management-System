daecfacf224b64f5bb9301f7f530411e5659dc81
porwalarchit <architporwal@gmail.com>
2023-05-29 18:36:19 +0530

    Service class for Employee report.

--- EMS-backend/src/main/java/com/archit/EMS/service/EmployeeReportService.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/EmployeeReportService.java
@@ -1,4 +1,11 @@
 package com.archit.EMS.service;
 
+import com.archit.EMS.entity.EmployeeReport;
+
+import java.util.Optional;
+
 public interface EmployeeReportService {
+    EmployeeReport saveReport(EmployeeReport theEmployeeReport);
+
+    Optional<EmployeeReport> getEmployeeReport(int id);
 }
--- EMS-backend/src/main/java/com/archit/EMS/service/EmployeeReportServiceImpl.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/EmployeeReportServiceImpl.java
@@ -1,4 +1,24 @@
 package com.archit.EMS.service;
 
-public class EmployeeReportServiceImpl {
+import com.archit.EMS.entity.EmployeeReport;
+import com.archit.EMS.repository.EmployeeReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeReportServiceImpl implements  EmployeeReportService{
+    @Autowired
+    EmployeeReportRepository employeeReportRepository;
+
+    @Override
+    public EmployeeReport saveReport(EmployeeReport theEmployeeReport) {
+        return employeeReportRepository.save(theEmployeeReport);
+    }
+
+    @Override
+    public Optional<EmployeeReport> getEmployeeReport(int id) {
+        return employeeReportRepository.findById(id);
+    }
 }
