93df490191ffa8763bb73d546b896ea1d024cea7
porwalarchit <architporwal@gmail.com>
2023-05-29 18:34:51 +0530

    Changed JWT token expiration time.

--- EMS-backend/src/main/java/com/archit/EMS/service/JwtService.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/JwtService.java
@@ -63,7 +63,7 @@ public class JwtService {
               .setSubject(email)
               .claim("role", employeeService.findEmployeeByEmail(email).get().getRoles().toString())
               .setIssuedAt(new Date(System.currentTimeMillis()))
-              .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+              .setExpiration(new Date(System.currentTimeMillis() + 86400000))
               .signWith(getSignInKey(), SignatureAlgorithm.HS256)
               .compact();
     }
