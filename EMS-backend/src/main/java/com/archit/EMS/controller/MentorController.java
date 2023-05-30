e650f106d750350f5906c1a190163bf03b0c52ec
porwalarchit <architporwal@gmail.com>
2023-05-30 12:42:15 +0530

    Mentor controller having POST & GET endpoints.

---
+++ EMS-backend/src/main/java/com/archit/EMS/controller/MentorController.java
@@ -0,0 +1,31 @@
+package com.archit.EMS.controller;
+
+import com.archit.EMS.entity.Mentor;
+import com.archit.EMS.service.EmployeeService;
+import com.archit.EMS.service.MentorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/mentor")
+public class MentorController {
+    @Autowired
+    private MentorService mentorService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("")
+    public Mentor addMentorDetails(@RequestBody Mentor theMentor){
+        theMentor.setEmployee(employeeService.findEmployeeByEmail(theMentor.getEmployee().getEmail()).get());
+        return mentorService.saveMentorDetails(theMentor);
+    }
+
+    @GetMapping("")
+    public Optional<Mentor> getMentorDetails(@RequestBody Mentor theMentor){
+        return mentorService.getMentorDetailsById(theMentor.getMentorId());
+    }
+}
