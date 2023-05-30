d6cb48967ccf6dd8ec32ca4a97c14e33eddc22fa
porwalarchit <architporwal@gmail.com>
2023-05-30 14:34:42 +0530

    Mentor feedback service class.

--- EMS-backend/src/main/java/com/archit/EMS/service/MentorFeedbackService.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/MentorFeedbackService.java
@@ -1,4 +1,11 @@
 package com.archit.EMS.service;
 
+import com.archit.EMS.entity.MentorFeedback;
+
+import java.util.Optional;
+
 public interface MentorFeedbackService {
+    Optional<MentorFeedback> getFeedback(int id);
+
+    MentorFeedback saveFeedback(MentorFeedback mentorFeedback);
 }
--- EMS-backend/src/main/java/com/archit/EMS/service/MentorFeedbackServiceImpl.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/MentorFeedbackServiceImpl.java
@@ -1,4 +1,23 @@
 package com.archit.EMS.service;
 
+import com.archit.EMS.entity.MentorFeedback;
+import com.archit.EMS.repository.MentorFeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class MentorFeedbackServiceImpl implements MentorFeedbackService{
+    @Autowired
+    private MentorFeedbackRepository mentorFeedbackRepository;
+    @Override
+    public Optional<MentorFeedback> getFeedback(int id) {
+        return mentorFeedbackRepository.findById(id);
+    }
+
+    @Override
+    public MentorFeedback saveFeedback(MentorFeedback mentorFeedback) {
+        return mentorFeedbackRepository.save(mentorFeedback);
+    }
 }
