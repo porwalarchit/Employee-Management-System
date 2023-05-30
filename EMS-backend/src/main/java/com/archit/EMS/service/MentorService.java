78fba79b7e196356ae110d8cf1d5452263b6d870
porwalarchit <architporwal@gmail.com>
2023-05-30 12:41:36 +0530

    Mentor Service class having methods for saving the data and returning the mentor details by id.

--- EMS-backend/src/main/java/com/archit/EMS/service/MentorService.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/MentorService.java
@@ -1,4 +1,11 @@
 package com.archit.EMS.service;
 
+import com.archit.EMS.entity.Mentor;
+
+import java.util.Optional;
+
 public interface MentorService {
+    Mentor saveMentorDetails(Mentor theMentor);
+
+    Optional<Mentor> getMentorDetailsById(int id);
 }
--- EMS-backend/src/main/java/com/archit/EMS/service/MentorServiceImpl.java
+++ EMS-backend/src/main/java/com/archit/EMS/service/MentorServiceImpl.java
@@ -1,4 +1,24 @@
 package com.archit.EMS.service;
 
+import com.archit.EMS.entity.Mentor;
+import com.archit.EMS.repository.MentorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class MentorServiceImpl implements MentorService{
+    @Autowired
+    private MentorRepository mentorRepository;
+
+    @Override
+    public Mentor saveMentorDetails(Mentor theMentor) {
+        return mentorRepository.save(theMentor);
+    }
+
+    @Override
+    public Optional<Mentor> getMentorDetailsById(int id) {
+        return mentorRepository.findById(id);
+    }
 }
