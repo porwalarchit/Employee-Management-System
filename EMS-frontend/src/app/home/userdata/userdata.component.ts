170630ee931ed09336c685cefe3e1c55acc0e087
porwalarchit <architporwal@gmail.com>
2023-05-31 19:40:42 +0530

    User data component

--- EMS-frontend/src/app/home/userdata/userdata.component.ts
+++ EMS-frontend/src/app/home/userdata/userdata.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-userdata',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./userdata.component.css']
 })
 export class UserdataComponent {
+  userData: any = []
+  constructor(private userService: UserService){
 
+  }
+
+  ngOnInit(){
+    this.userService.getUserData().subscribe((res)=>{
+      // console.log(res);
+      this.userData = res;
+    })
+  }
 }
