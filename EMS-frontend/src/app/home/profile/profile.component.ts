545c47391bb2e084eda94dc98d7d1f1d64863c9e
porwalarchit <architporwal@gmail.com>
2023-06-01 12:23:14 +0530

    Show profile details of a user.

--- EMS-frontend/src/app/home/profile/profile.component.ts
+++ EMS-frontend/src/app/home/profile/profile.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/service/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileData: any = [];
+  isMale: boolean = true; // Condition based on your data
+  maleImageUrl: string = '../../../assets/images/male-img.png';
+  femaleImageUrl: string = '../../../assets/images/female-img.png';
+
+  constructor(private profile: ProfileService){
+
+  }
+
+  ngOnInit(){
+    this.profile.getProfile().subscribe((res)=>{
+      // console.log(res);
+      this.profileData = res;
+      console.log(this.profileData);
+      if(this.profileData.employeeDetails?.gender === 'Female'){
+        this.isMale = false;
+      }
+    })
+  }
+
+  ngOnChanges(){
+    this.profileData =  this.profile.getProfile();
+  }
 
 }
