ed3dc44c25a50fe6129132adeaa052704f091660
porwalarchit <architporwal@gmail.com>
2023-06-01 12:23:42 +0530

    Show specific buttons to user based on their role only.

--- EMS-frontend/src/app/home/home.component.ts
+++ EMS-frontend/src/app/home/home.component.ts
@@ -11,19 +11,49 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit{
   decodedData: any;
   loggedIn : Boolean = true;
+  userRole: string;
+  userEmployee: Boolean = false;
+  userAdmin : Boolean = false;
+  userSuperAdmin: Boolean = false;
 
-  constructor(private router: Router) {
+
+  constructor(private router: Router, private role : RoleService) {
   }
 
   ngOnInit(){
     if(localStorage.getItem('token')){
       this.loggedIn = false;
     } 
+    this.userRole = this.role.getRole();
+    this.checkUser();
   }
 
   logout(){
     localStorage.removeItem('token');
     this.loggedIn = true;
     this.router.navigate(['/login']);
+  }
+
+  checkUser(){
+    if(this.userRole === 'ROLE_EMPLOYEE'){
+      this.userEmployee = true;
+      this.userAdmin = false;
+      this.userSuperAdmin = false;
+    }
+    else if(this.userRole === 'ROLE_ADMIN'){
+      this.userEmployee = false;
+      this.userAdmin = true;
+      this.userSuperAdmin = false;
+    }
+    else if(this.userRole === 'ROLE_SUPERADMIN'){
+      this.userEmployee = false;
+      this.userAdmin = false;
+      this.userSuperAdmin = true;
+    }
+    else{
+      this.userEmployee = false;
+      this.userAdmin = false;
+      this.userSuperAdmin = false;
+    }
   }
 }
