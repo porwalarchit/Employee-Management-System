4c79e24e9a71cf5edcf46c8e220e7b3db2cdb0d1
porwalarchit <architporwal@gmail.com>
2023-06-05 15:11:12 +0530

    Route guard service class added for Authentication.

--- EMS-frontend/src/app/app-routing.module.ts
+++ EMS-frontend/src/app/app-routing.module.ts
@@ -6,15 +6,20 @@ import { RegisterComponent } from './register/register.component';
 import { UserdataComponent } from './home/userdata/userdata.component';
 import { ProfileComponent } from './home/profile/profile.component';
 import { ErrorComponent } from './error/error.component';
+import { AuthGuardService } from './route-guard/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', redirectTo:'profile',pathMatch:'full'},
+  {path: '', canActivate: [AuthGuardService], redirectTo:'profile',pathMatch:'full'},
   { path: '', component: HomeComponent, children: [
-    { path: 'profile', component: ProfileComponent},
-    { path: 'register', component: RegisterComponent },
-    { path: 'userdata', component: UserdataComponent }
+    { path: 'profile', canActivate: [AuthGuardService],component: ProfileComponent},
+    { path: 'register', canActivate: [AuthGuardService] , component: RegisterComponent },
+    { path: 'userdata', canActivate: [AuthGuardService] ,component: UserdataComponent },
   ] },
   { path: 'login', component: LoginComponent },
+  // {path: '',redirectTo:'profile',pathMatch:'full'},
+  // { path: 'profile', canActivate: [AuthGuardService],component: ProfileComponent},
+  // { path: 'register', canActivate: [AuthGuardService], component: RegisterComponent },
+  // { path: 'userdata', canActivate: [AuthGuardService], component: UserdataComponent },
   {path: '**', component: ErrorComponent}
 ];
 
--- EMS-frontend/src/app/route-guard/auth-guard.service.ts
+++ EMS-frontend/src/app/route-guard/auth-guard.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    console.log(state.url);
+    
+      // if(state.url == "/login"){
+      //   localStorage.removeItem('token');
+      //   return true;
+      // }
+
+      let token = localStorage.getItem('token');
+      if(!token){
+        this.router.navigate(['/login']);
+        return false;
+      }
+      else{
+        this.router.navigate(['/profile']);
+        return true;
+      }
+    }
 }
