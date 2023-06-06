0d70009984074e3bb6ea04d35dd274ca91737b1d
Archit Porwal <aporwal@argusoft.com>
2023-06-06 23:49:12 +0530

    Employee Id DTO

---
+++ EMS-backend/src/main/java/com/archit/EMS/dto/EmployeeId.java
@@ -0,0 +1,20 @@
+package com.archit.EMS.dto;
+
+public class EmployeeId {
+    private int id;
+
+    public EmployeeId() {
+    }
+
+    public EmployeeId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
