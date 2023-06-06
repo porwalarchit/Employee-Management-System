3d8c62b03bcfd953130a4678df629c4cca92004f
Archit Porwal <aporwal@argusoft.com>
2023-06-06 16:57:03 +0530

    Minor change

--- EMS-frontend/src/app/service/user.service.ts
+++ EMS-frontend/src/app/service/user.service.ts
@@ -13,12 +13,6 @@ export class UserService {
   BASE_URL = "http://localhost:8080";
 
   getUserData(){
-    // const token = localStorage.getItem('token');
-    // console.log("TOKEN: ", token);
-
-    // Create the request headers with the bearer token
-    // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    // console.log("HEADER: ", headers);
     return this.http.get(this.BASE_URL + '/api/findAllEmployees');
   }
 }
