6967de933e668a285040c7a096ecc01d93921850
Archit Porwal <aporwal@argusoft.com>
2023-06-06 16:58:30 +0530

    Update profile component.

--- EMS-frontend/src/app/home/update-profile/update-profile.component.ts
+++ EMS-frontend/src/app/home/update-profile/update-profile.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, AfterViewInit, ViewChild, ElementRef, OnInit  } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ProfileService } from 'src/app/service/profile.service';
+import { UpdateProfileService } from 'src/app/service/update-profile.service';
 
 @Component({
   selector: 'app-update-profile',
@@ -13,7 +16,9 @@ export class UpdateProfileComponent implements OnInit{
   maleImageUrl: string = '../../../assets/images/male-img.png';
   femaleImageUrl: string = '../../../assets/images/female-img.png';
 
-  constructor(private profile: ProfileService){
+  updateDetailsForm: FormGroup;
+
+  constructor(private profile: ProfileService, private http: HttpClient, private updateprofile: UpdateProfileService){
   }
 
   ngOnInit(){
@@ -24,7 +29,36 @@ export class UpdateProfileComponent implements OnInit{
       if(this.profileData.employeeDetails?.gender === 'Female'){
         this.isMale = false;
       }
+      this.updateDetailsForm = new FormGroup({
+        gender: new FormControl(this.profileData.employeeDetails?.gender),
+        dateOfBirth: new FormControl(this.profileData.employeeDetails?.dateOfBirth),
+        contactNumber: new FormControl(this.profileData.employeeDetails?.contactNumber),
+        joiningDate: new FormControl(this.profileData.employeeDetails?.joiningDate)
+      })
     })
+
+    
+  }
+
+  onSubmit(){
+
+    const updatedDetails = {
+      gender: this.updateDetailsForm.value.gender,
+      dateOfBirth: this.updateDetailsForm.value.dateOfBirth,
+      contactNumber: this.updateDetailsForm.value.contactNumber,
+      joiningDate: this.updateDetailsForm.value.joiningDate,
+      employee: {email: this.profileData.email}
+    }
+
+    console.log(updatedDetails);
+    this.updateprofile.updateProfile(updatedDetails).subscribe(
+      (res)=>{
+        console.log(res);
+      }, (err)=>{
+        console.log(err);
+        
+      }
+    );
   }
 
 }
