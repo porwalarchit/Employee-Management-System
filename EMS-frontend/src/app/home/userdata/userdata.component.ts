f3c66cc9541a81f051bb43036ed78b712122f544
porwalarchit <architporwal@gmail.com>
2023-06-07 18:53:06 +0530

    User data component having a  edit button for updating designation.

--- EMS-frontend/src/app/home/userdata/userdata.component.ts
+++ EMS-frontend/src/app/home/userdata/userdata.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -7,15 +8,42 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./userdata.component.css']
 })
 export class UserdataComponent {
-  userData: any = []
-  constructor(private userService: UserService){
+  userData: any = [];
+
+
+  isEditing: boolean = false;
+  designationValue: string = '';
+
+  constructor(private userService: UserService, private route: Router) {
 
   }
 
-  ngOnInit(){
-    this.userService.getUserData().subscribe((res)=>{
-      // console.log(res);
+  ngOnInit() {
+    this.userService.getUserData().subscribe((res) => {
       this.userData = res;
     })
   }
+
+  editDetails(userId) {
+    this.route.navigate(['userdata/update', userId]);
+  }
+
+
+  enableEdit(data) {
+    data.isEditing = true;
+    data.designationValue = data.employeeDetails?.designation;
+  }
+
+  saveEdit(data) {
+    data.isEditing = false;
+    console.log(this.designationValue);
+    
+    console.log(data);
+    // Perform save logic here, such as making an API call or updating the data locally
+  }
+
+  cancelEdit(data) {
+    data.isEditing = false;
+  }
+
 }
