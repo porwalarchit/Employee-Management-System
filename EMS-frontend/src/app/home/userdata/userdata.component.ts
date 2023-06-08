1b62cd3ec235664303eadd959c6400d1fe917246
Archit Porwal <aporwal@argusoft.com>
2023-06-08 12:53:08 +0530

    Update user designation by admin functionality done.

--- EMS-frontend/src/app/home/userdata/userdata.component.ts
+++ EMS-frontend/src/app/home/userdata/userdata.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UpdateProfileService } from 'src/app/service/update-profile.service';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -14,13 +15,15 @@ export class UserdataComponent {
   isEditing: boolean = false;
   designationValue: string = '';
 
-  constructor(private userService: UserService, private route: Router) {
+  constructor(private userService: UserService, private updateService: UpdateProfileService,private route: Router) {
 
   }
 
   ngOnInit() {
     this.userService.getUserData().subscribe((res) => {
       this.userData = res;
+      console.log(this.userData);
+      
     })
   }
 
@@ -34,12 +37,42 @@ export class UserdataComponent {
     data.designationValue = data.employeeDetails?.designation;
   }
 
-  saveEdit(data) {
+  saveEdit(data,i) {
     data.isEditing = false;
-    console.log(this.designationValue);
+    // console.log(i);
     
-    console.log(data);
-    // Perform save logic here, such as making an API call or updating the data locally
+    if(data.employeeDetails){
+      const updatedDetails = {
+        empMdId: data.employeeDetails?.empMdId,
+        gender: data.employeeDetails?.gender,
+        dateOfBirth: data.employeeDetails?.dateOfBirth,
+        contactNumber: data.employeeDetails?.contactNumber,
+        joiningDate: data.employeeDetails?.joiningDate,
+        designation: data.employeeDetails.designation
+      }
+      this.updateService.updateProfile(updatedDetails).subscribe(
+        (res)=>{
+          this.userData[i] = data;
+          console.log(res);
+        }, (err) => {
+          console.log(err);
+        }
+      )
+    }
+    else{
+      const newDetails = {
+        designation: data.employeeDetails.designation,
+        employee: {email: data.email}
+      }
+      this.updateService.addDetails(newDetails).subscribe(
+        (res) => {
+          this.userData[i] = data;
+          console.log(res);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+    }
   }
 
   cancelEdit(data) {
