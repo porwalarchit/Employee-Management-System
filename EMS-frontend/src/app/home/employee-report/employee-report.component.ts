f728a9f3ba3059c6b65be6a28821893c13c56472
Archit Porwal <aporwal@argusoft.com>
2023-06-14 10:32:42 +0530

    Employee report component.

---
+++ EMS-frontend/src/app/home/employee-report/employee-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-report',
+  templateUrl: './employee-report.component.html',
+  styleUrls: ['./employee-report.component.css']
+})
+export class EmployeeReportComponent {
+
+}
