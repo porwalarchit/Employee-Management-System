4cbda6afe78cdf95effdf1ad4c33908d01b3acf6
Archit Porwal <aporwal@argusoft.com>
2023-06-16 12:47:29 +0530

    Added an method along with a query to get all reports based on the employee Id.

--- EMS-backend/src/main/java/com/archit/EMS/repository/EmployeeReportRepository.java
+++ EMS-backend/src/main/java/com/archit/EMS/repository/EmployeeReportRepository.java
@@ -2,8 +2,14 @@ package com.archit.EMS.repository;
 
 import com.archit.EMS.entity.EmployeeReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeReportRepository extends JpaRepository<EmployeeReport, Integer> {
+    @Query("SELECT e FROM EmployeeReport e WHERE e.employee.id=:empId")
+    List<EmployeeReport> findReportByEmpId(@Param("empId") int empId);
 }
