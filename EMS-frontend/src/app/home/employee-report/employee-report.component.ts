fe49a5bb4f200741cf4c6ff231b11da62b3317e4
Archit Porwal <aporwal@argusoft.com>
2023-06-16 12:51:44 +0530

    Added employee report component to get all employee reports.

--- EMS-frontend/src/app/home/employee-report/employee-report.component.ts
+++ EMS-frontend/src/app/home/employee-report/employee-report.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { AddEmployeeReportComponent } from '../add-employee-report/add-employee-report.component';
+import { EmployeeReportService } from 'src/app/service/employee-report.service';
 
 @Component({
   selector: 'app-employee-report',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-report.component.css']
 })
 export class EmployeeReportComponent {
+  ngbModalRef: NgbModalRef;
+  reportData: any;
 
+  constructor(private employeeReportService: EmployeeReportService, private modalService: NgbModal){}
+
+  ngOnInit(){
+    const empId = localStorage.getItem('empId');
+    this.employeeReportService.getAllReportsById(empId).subscribe(
+      (res)=>{
+        this.reportData = res;
+        console.log(res);
+      }, (err)=>{
+        console.log(err);
+      }
+    )
+  }
+
+  onClick(){
+    this.ngbModalRef = this.modalService.open(AddEmployeeReportComponent);
+    this.ngbModalRef.componentInstance.reports = this.reportData;
+  }
 }
