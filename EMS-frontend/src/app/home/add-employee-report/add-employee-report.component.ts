294cc0d7e799b2dae9b94173d71f708b046ded5e
Archit Porwal <aporwal@argusoft.com>
2023-06-19 18:28:44 +0530

    Added functionality to set default date to taday, & removed the functiality to set reportStatus  by the user.

--- EMS-frontend/src/app/home/add-employee-report/add-employee-report.component.ts
+++ EMS-frontend/src/app/home/add-employee-report/add-employee-report.component.ts
@@ -16,24 +16,27 @@ export class AddEmployeeReportComponent {
   empEmail: String;
   @Input() reports;
 
-  constructor(private employeeReportService: EmployeeReportService, private formBuilder: FormBuilder ,private route: Router ,private ngbActiveModal: NgbActiveModal, private toastr: ToastrService){}
+  constructor(private employeeReportService: EmployeeReportService, 
+    private ngbActiveModal: NgbActiveModal,
+    private toastr: ToastrService
+    ){}
 
   ngOnInit(){
-    this.employeeReportForm = this.formBuilder.group({
-      reportingDate: [''],
-      workDone: [''],
-      reportStatus: ['Pending']
-    });
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+    const day = ('0' + currentDate.getDate()).slice(-2);
+    const todayDate = `${year}-${month}-${day}`;
 
     this.empEmail = jwt_decode(localStorage.getItem('token'))['sub'];
 
     this.employeeReportForm = new FormGroup({
-      reportingDate: new FormControl("", Validators.required),
+      reportingDate: new FormControl(todayDate, Validators.required),
       workDone: new FormControl("", Validators.required),
-      reportStatus: new FormControl("")
+      reportStatus: new FormControl("Pending")
     })
   }
-
+  
   
   submitReport(){
     const reportDetails = {
