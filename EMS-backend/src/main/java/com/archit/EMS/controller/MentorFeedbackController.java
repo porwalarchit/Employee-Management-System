46a2ee4baf25a7cfbafdd38168ca7fa3fc77d5dc
Archit Porwal <aporwal@argusoft.com>
2023-06-19 18:22:37 +0530

    Added a PUT request endpoint for updating mentor feedback.

--- EMS-backend/src/main/java/com/archit/EMS/controller/MentorFeedbackController.java
+++ EMS-backend/src/main/java/com/archit/EMS/controller/MentorFeedbackController.java
@@ -1,6 +1,8 @@
 package com.archit.EMS.controller;
 
+import com.archit.EMS.entity.EmployeeReport;
 import com.archit.EMS.entity.MentorFeedback;
+import com.archit.EMS.service.EmployeeReportService;
 import com.archit.EMS.service.MentorFeedbackService;
 import com.archit.EMS.service.MentorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +20,45 @@ public class MentorFeedbackController {
     @Autowired
     private MentorService mentorService;
 
+    @Autowired
+    private EmployeeReportService employeeReportService;
+
+    @GetMapping("{id}")
+    public  MentorFeedback getFeedback(@PathVariable int id){
+        return mentorFeedbackService.getFeedback(id).get();
+    }
+
     @PostMapping("")
     public MentorFeedback saveFeedback(@RequestBody MentorFeedback theMentorFeedback){
         theMentorFeedback.setMentor(mentorService.getMentorDetailsById(theMentorFeedback.getMentor().getMentorId()).get());
-//        theMentorFeedback.setMentor();
-        return theMentorFeedback;
-//        return mentorFeedbackService.saveFeedback(theMentorFeedback);
+        EmployeeReport emp = employeeReportService.getEmployeeReport(theMentorFeedback.getEmployeeReport().getReportId()).get();
+        if(theMentorFeedback.getFlag()){
+            emp.setReportStatus("Rejected");
+        }
+        else{
+            emp.setReportStatus("Accepted");
+        }
+        theMentorFeedback.setEmployeeReport(emp);
+        return mentorFeedbackService.saveFeedback(Optional.of(theMentorFeedback));
     }
 
-    @GetMapping("{id}")
-    public Optional<MentorFeedback> getFeedback(@PathVariable int id){
-        return mentorFeedbackService.getFeedback(id);
+    @PutMapping("")
+    public MentorFeedback updateFeedback(@RequestBody MentorFeedback theMentorFeedback){
+        Optional<MentorFeedback> tempFeedback = mentorFeedbackService.getFeedback(theMentorFeedback.getFeedbackId());
+
+        tempFeedback.get().setFeedbackId(theMentorFeedback.getFeedbackId());
+        tempFeedback.get().setFeedback(theMentorFeedback.getFeedback());
+        tempFeedback.get().setFlag(theMentorFeedback.getFlag());
+        tempFeedback.get().setMentor(mentorService.getMentorDetailsById(theMentorFeedback.getMentor().getMentorId()).get());
+
+        EmployeeReport emp = employeeReportService.getEmployeeReport(theMentorFeedback.getEmployeeReport().getReportId()).get();
+        if(theMentorFeedback.getFlag()){
+            emp.setReportStatus("Rejected");
+        }
+        else{
+            emp.setReportStatus("Accepted");
+        }
+        tempFeedback.get().setEmployeeReport(emp);
+        return mentorFeedbackService.saveFeedback(tempFeedback);
     }
 }
